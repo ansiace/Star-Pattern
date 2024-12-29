638c4d03cec897c91b824abc0e842a0e591d532c
Animesh Raj <animeshraj80707@gmail.com>
2024-12-29 14:39:19 +0530

    Update starpattern1.cpp

--- starpattern1.cpp
+++ starpattern1.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
 using namespace std;
-int main()
-// without input
-{
-    for(int i=1;i<=5;i++)
-    {
-        for(int j=1;j<=5;j++)
-        {
-            if(j<=i)
-            {
+int main() {
+
+    int i , j , n ;
+    cin>>n;
+
+    for ( int i = 1 ; i <= n ; i++ ){
+        
+        for ( int j = 1 ; j <= n ; j++ ){
+            
+            if( j <= i ){
                 cout<<"*";
             }
-            else
-            {
+            else{
                 cout<<" ";
             }
         }
+       
         cout<<endl;
-        
     }
-    
+
 }
