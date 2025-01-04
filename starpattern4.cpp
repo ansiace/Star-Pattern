f7ecaa65bb7498e451e95ad99baef43effa4b6af
Animesh Raj <animeshraj80707@gmail.com>
2025-01-04 19:15:20 +0530

    Update starpattern4.cpp

--- starpattern4.cpp
+++ starpattern4.cpp
@@ -1,22 +1,23 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-    for(int i=1;i<=5;i++)
-    {
-        for(int j=1;j<=5;j++)
-        {
-            if(j<=6-i)
-            {
+int main() {
+
+    int i , j , n ;
+    cin>>n;
+
+    for ( int i = 1 ; i <= n ; i++ ){
+        
+        for ( int j = 1 ; j <= n ; j++ ){
+            
+            if( j <= 6-i ){
                 cout<<"*";
             }
-            else
-            {
+            else{
                 cout<<" ";
             }
         }
+       
         cout<<endl;
-        
     }
-    
+
 }
