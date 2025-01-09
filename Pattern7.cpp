cae107db55d27f0018c931b9991de834faeb7a00
Animesh Raj <animeshraj80707@gmail.com>
2025-01-09 19:05:21 +0530

    Add files via upload

---
+++ Pattern7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 5 , j = 9 ;
+
+    for ( i = 1 ; i <= 5 ; i++ ){
+        for ( j = 1 ; j <= 9 ; j++ ){
+            if ( j <= 6-i || j >= 4+i ){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
